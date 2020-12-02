17863f82bd425c9ed8e16bb081a3f956454cd999
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-02 20:59:09 +0000

    parsed input to own file and exp/req'd it

--- play.js
+++ play.js
@@ -1,39 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-
-const setupInput = function(conn) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = function() {
-    stdin.on('data', key => {
-      if (key === 'w') {
-        conn.write('Move: up');
-      }
-      if (key === 'a') {
-        conn.write('Move: left');
-      }
-      if (key === 's') {
-        conn.write('Move: down');
-      }
-      if (key === 'd') {
-        conn.write('Move: right');
-      }
-      if (key === '\u0003') {
-        process.stdout.write(`Thanks for using me, ciao!\n`);
-        process.exit();
-      }
-    });
-  };
-
-  handleUserInput();
-
-  return stdin;
-};
-
 setupInput();
-
