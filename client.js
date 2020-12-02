f0669c2c95614caf04abeb158cbf0bc883daac5e
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-02 20:10:24 +0000

    named snake

--- client.js
+++ client.js
@@ -1,16 +1,22 @@
 const net = require('net');
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', data => { 
-    console.log('you ded cuz you idled', data)
-  })
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    conn.write('Name: AKT');
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('data', data => {
+    console.log('Removed from game for: ', data);
+  });
 
   return conn;
-}
+};
 
 module.exports = { connect };
